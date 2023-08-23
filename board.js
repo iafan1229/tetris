104767d1bc6002f48bd910355321ade6c5487648
hayounglee <home0@naver.com>
2023-08-23 14:22:09 +0900

    fix: 파일 정리(canvas.js 지움)

--- board.js
+++ board.js
@@ -2,6 +2,14 @@
 import { constant } from './constant.js';
 import { initPiece as piece } from './piece.js';
 
+export function setCanvas(canvas, ctx) {
+	let x = 0;
+	let y = 0;
+	canvas.width = constant.ROW * constant.double;
+	canvas.height = constant.COLUMN * constant.double;
+	drawBoard(x, y, ctx);
+}
+
 export function drawBoard(x, y, ctx) {
 	ctx.clearRect(
 		0,
@@ -23,4 +31,23 @@ export function drawBoard(x, y, ctx) {
 			}
 		});
 	});
+}
+
+export function move() {
+	// 화살표 키 입력을 감지하고 블록을 이동합니다.
+	// (이 부분은 원하는 게임 논리로 변경해야 합니다)
+	let x = 0;
+	let y = 0;
+	return function moveClosure(event) {
+		if (event.key === 'ArrowLeft') {
+			x -= 10; // 왼쪽으로 이동
+		} else if (event.key === 'ArrowRight') {
+			x += 10; // 오른쪽으로 이동
+		} else if (event.key === 'ArrowUp') {
+			y -= 10; // 위로 이동
+		} else if (event.key === 'ArrowDown') {
+			y += 10; // 아래로 이동
+		}
+		return { x, y };
+	};
 }
--- main.js
+++ main.js
@@ -1,18 +1,21 @@
-// main.js
-import { setCanvas, move } from './canvas.js';
-import { constant } from './constant.js';
-import { initPiece } from './piece.js';
 import { drawBoard } from './board.js';
+import { setCanvas, move } from './board.js';
 
 const canvas = document.getElementById('myCanvas');
 const ctx = canvas.getContext('2d');
 
 setCanvas(canvas, ctx);
+canvas.tabIndex = 1;
+
+// 페이지가 로드될 때 canvas에 포커스를 주기
+window.addEventListener('load', () => {
+	canvas.focus();
+});
 
 const saveMovefunction = move();
 // 키보드 누를 때마다 방향이 바뀌도록 이벤트 리스너를 추가합니다.
 canvas.addEventListener('keydown', (event) => {
 	const { x, y } = saveMovefunction(event, ctx);
-	console.log(x);
+
 	drawBoard(x, y, ctx);
 });
