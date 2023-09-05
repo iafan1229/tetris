799cd2d11f91920cbda0255f0a94f2d012935954
iafan1229 <home0@naver.com>
2023-09-05 10:24:34 +0900

    fix: 블록 쌓이게 개발

--- canvas.js
+++ canvas.js
@@ -21,25 +21,49 @@ export class Canvas {
     );
     return this.grid;
   }
+  drawBoard() {
+    this.grid.forEach((row, y) => {
+      row.forEach((value, x) => {
+        if (value >= 1) {
+          this.ctx.fillStyle = Object.values(constant.colors[value - 1])[0];
+          this.ctx.fillRect(x, y, 1, 1);
+        }
+      });
+    });
+  }
   draw(x, y, ctx, block) {
     if (this.isValid()) {
-      ctx.clearRect(0, 0, constant.COLUMN, constant.ROW);
+      ctx.clearRect(0, 0, constant.ROW, constant.COLUMN);
       // console.log(block);
+      this.xDirection = [];
+      this.yDirection = [];
       block.forEach((row, columnIdx) => {
         row.forEach((el, rowIdx) => {
           this.xDirection.push(x + rowIdx);
           this.yDirection.push(y + columnIdx);
-
-          if (el >= 1) {
-            console.log(el);
+          if (this.yDirection.some((el) => Math.abs(el) === 11)) {
+            this.freeze();
+            // const newBlock = this.freeze(array);
+            // this.blockArray.push(newBlock);
+            this.blockCount += 1;
+            //-----초기화
+            this.x = 3;
+            this.y = 0;
+            this.xDirection = [];
+            this.yDirection = [];
+            this.keyFlag = [0, 0, 0, 0];
+            // array = JSON.parse(JSON.stringify([...newBlock]));
+            this.rotations = 0;
+            //------초기화 끝
+          } else if (el >= 1) {
             let colorVariable = Object.values(constant.colors[el - 1])[0];
             ctx.fillStyle = colorVariable;
-
             ctx.fillRect(x + rowIdx, y + columnIdx, 1, 1);
           }
         });
       });
     }
+    this.drawBoard();
   }
   animate(now, time) {
     // 지난 시간을 업데이트한다.
@@ -48,30 +72,12 @@ export class Canvas {
     if (time.elapsed > time.level) {
       // 현재 시간을 다시 측정한다.
       time.start = now;
-      // this.ctx.clearRect(0, 0, constant.ROW, constant.COLUMN);
       this.y += 1;
-      if (this.yDirection.some((el) => Math.abs(el) > 360)) {
-        this.freeze(x, y);
-        const newBlock = freeze(array);
-        this.blockArray.push(newBlock);
-        this.blockCount += 1;
-        //-----초기화
-        this.x = 3;
-        this.y = 0;
-        this.xDirection = [];
-        this.yDirection = [];
-        this.keyFlag = [0, 0, 0, 0];
-        // array = JSON.parse(JSON.stringify([...newBlock]));
-        this.rotations = 0;
-        //------초기화 끝
-      }
 
       this.draw(this.x, this.y, this.ctx, this.piece);
     }
 
-    const requestId = requestAnimationFrame(() =>
-      this.animate(Date.now(), constant.time)
-    );
+    requestAnimationFrame(() => this.animate(Date.now(), constant.time));
   }
   move(event) {
     // 화살표 키 입력을 감지하고 블록을 이동합니다.
@@ -79,21 +85,21 @@ export class Canvas {
 
     if (event.key === "ArrowLeft") {
       this.keyFlag = [1, 0, 0, 0];
-      if (canvas.isValid()) {
+      if (this.isValid()) {
         this.x -= 10;
       } // 왼쪽으로 이동
     } else if (event.key === "ArrowRight") {
       this.keyFlag = [0, 1, 0, 0];
-      if (canvas.isValid()) {
+      if (this.isValid()) {
         this.x += 10; // 오른쪽으로 이동
       }
     } else if (event.key === "ArrowUp") {
       // this.keyFlag = [0, 0, 1, 0];
-      canvas.rotateArrayClockwise();
-      // if (canvas.isValid()) y -= 10; // 위로 이동
+      this.rotateArrayClockwise();
+      // if (this.isValid()) y -= 10; // 위로 이동
     } else if (event.key === "ArrowDown") {
       this.keyFlag = [0, 0, 0, 1];
-      if (canvas.isValid()) this.y += 10; // 아래로 이동
+      if (this.isValid()) this.y += 10; // 아래로 이동
     }
     this.key = event.key;
     return { x: this.x, y: this.y };
@@ -101,42 +107,39 @@ export class Canvas {
   isValid() {
     //왼쪽 끝에 도달하고 [1,0,0,0]임
     if (
-      this.xDirection.some((el) => Math.abs(el) === 10) &&
+      this.xDirection.some((el) => Math.abs(el) === 0) &&
       keyFlag.findIndex((el) => el === 1) === 0
     ) {
       return false;
     }
     //오른쪽 끝에 도달
     if (
-      this.xDirection.some((el) => Math.abs(el) === 360) &&
+      this.xDirection.some((el) => Math.abs(el) === 270) &&
       keyFlag.findIndex((el) => el === 1) === 1
     ) {
       return false;
     }
-    if (
-      this.yDirection.some((el) => Math.abs(el) === 800) &&
-      keyFlag.findIndex((el) => el === 1) === 3
-    ) {
-      return false;
-    }
+    // if (
+    //   this.yDirection.some((el) => Math.abs(el) === 360) &&
+    //   keyFlag.findIndex((el) => el === 1) === 3
+    // ) {
+    //   return false;
+    // }
 
     return true;
   }
-  freeze(x, y) {
+  freeze() {
     // this.piece = [...array];
-    this.piece.forEach((row, columnIdx) => {
-      row.forEach((el, rowIdx) => {
-        if (el >= 1) {
+    this.piece.forEach((row, y) => {
+      row.forEach((value, x) => {
+        if (value >= 1) {
           // 현재 블록의 각 셀을 게임 보드에 고정
-          console.table(this.piece); //0+9-3
-          const boardY = y + columnIdx;
-          const boardX = x + rowIdx;
-          console.log(boardY);
           // 게임 보드에 값을 설정 (9는 블록이 차있는 상태를 나타냄)
-          this.grid[boardX][boardY] = el;
+          this.grid[y + this.y][x + this.x] = value;
         }
       });
     });
+    console.log(this.grid);
     // this.piece = array;
   }
   rotateArrayClockwise() {
